e72f30aa2a040d426eb348358ba731f13cdf82ba
LucasLaza <lucasv.lazarin@gmail.com>
2025-05-10 19:42:28 -0300

    Adding final logic.

--- trabalho.js
+++ trabalho.js
@@ -1,21 +1,39 @@
-function geradorDeTagsDeIdentificacao() {
-return 
+function geradorDeTagsDeIdentificacao(nome) {
+return nome.toUpperCase(); 
 }
 
-function verificarSePodeSerAdotado() {
-return
+function verificarSePodeSerAdotado(idade, porte) {
+  const idadepode = idade <= 2;
+
+  const portepode = porte === 'P';
+
+  if (idadepode || portepode) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function calcularConsumoDeRacao() {
-return
+function calcularConsumoDeRacao(nome, idade, peso) {
+  const consumoPorKg = 300;
+  const total = peso * consumoPorKg;
+  return total;
 }
 
-function decidirTipoDeAtividadePorPorte() {
-return
+function decidirTipoDeAtividadePorPorte(porte) {
+  if (porte === 'pequeno') {
+    return 'brincar dentro de casa';
+  } else if (porte === 'medio') {
+    return 'caminhada no quarteirão';
+  } else if (porte === 'grande') {
+    return 'correr no parque';
+  } else {
+    return 'porte inválido';
+  }
 }
 
 async function buscarDadoAsync() {
-return
+  return 'Pipoca';
 }
 
 export {
